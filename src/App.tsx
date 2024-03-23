b9201f4815026c62b8fb3825cbe235b17d8a81d4
Juan Gaona <jsgaonac@protonmail.com>
2024-03-23 14:12:49 -0500

    Add basic logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import {useRef, useState} from 'react';
 import './App.css'
 
 function App() {
+    const [isProcessing, setIsProcessing] = useState(false);
+    const dataIn = useRef(null);
+    const dataOut = useRef(null);
+
+    function transform() {
+        setIsProcessing(true);
+
+        dataOut.current.value = dataIn.current.value;
+
+        setIsProcessing(false);
+    }
+
     return (
         <>
             <h1>Wherify</h1>
@@ -8,11 +21,15 @@ function App() {
             <section className="content">
                 <div className="content-areas">
                     <textarea name="data-in" id="data-in" rows={20}
-                              placeholder="Paste your data here..."></textarea>
+                              placeholder="Paste your data here..."
+                              ref={dataIn}></textarea>
                     <textarea name="data-out" id="data-out" rows={20}
-                              placeholder="Output goes here..."></textarea>
+                              placeholder="Output goes here..."
+                              ref={dataOut}></textarea>
                 </div>
-                <button id="transform-btn">Get it</button>
+                <button id="transform-btn" onClick={transform} disabled={isProcessing}>
+                    { isProcessing ? "Working on it..." : "Transform" }
+                </button>
             </section>
         </>
     )
