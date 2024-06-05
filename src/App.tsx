1cf7a50da8378c829c7d82dac6c8e197aa3042e2
Juan Gaona <jsgaonac@protonmail.com>
2024-06-04 19:57:44 -0500

    Add options and implemented functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,33 @@ function App() {
     const [isProcessing, setIsProcessing] = useState(false);
     const dataIn = useRef<HTMLTextAreaElement>(null);
     const dataOut = useRef<HTMLTextAreaElement>(null);
+    const [isAddParenthesis, setIsAddParenthesis] = useState(true);
+    const [isAddBrackets, setIsAddBrackets] = useState(false);
+    const [isSingleQuotes, setIsSingleQuotes] = useState(true);
+    const [isDoubleQuotes, setIsDoubleQuotes] = useState(false);
 
     function transform() {
         setIsProcessing(true);
 
         if (dataOut.current !== null) {
-            dataOut.current.value = dataIn.current?.value ?? '';
+            let surroundingChar = '';
+
+            if (isSingleQuotes) {
+                surroundingChar = '\'';
+            } else if (isDoubleQuotes) {
+                surroundingChar = '"';
+            }
+
+            const words = (dataIn.current?.value?.trim()?.split(/\s+/) || []).filter(w => w != ',');
+            let processedWords = words.map(w => `${surroundingChar}${w}${surroundingChar}`).join('\n,');
+
+            if (isAddParenthesis) {
+                processedWords = `(\n${processedWords}\n)`;
+            } else if (isAddBrackets) {
+                processedWords = `[\n${processedWords}\n]`;
+            }
+
+            dataOut.current.value = processedWords;
         }
 
         setIsProcessing(false);
@@ -29,8 +50,39 @@ function App() {
                               placeholder="Output goes here..."
                               ref={dataOut}></textarea>
                 </div>
+
+                <section className="options">
+                    <label htmlFor="add-par">Use single-quotes
+                        <input type="checkbox" checked={isSingleQuotes} onChange={(e) => {
+                            setIsSingleQuotes(e.target.checked);
+                            setIsDoubleQuotes(false);
+                        }}/>
+                    </label>
+
+                    <label htmlFor="add-par">Use double-quotes
+                        <input type="checkbox" checked={isDoubleQuotes} onChange={(e) => {
+                            setIsSingleQuotes(false);
+                            setIsDoubleQuotes(e.target.checked);
+                        }}/>
+                    </label>
+
+                    <label htmlFor="add-par">Add parenthesis
+                        <input type="checkbox" checked={isAddParenthesis} onChange={(e) => {
+                            setIsAddParenthesis(e.target.checked);
+                            setIsAddBrackets(false);
+                        }}/>
+                    </label>
+
+                    <label htmlFor="add-par">Add brackets
+                        <input type="checkbox" checked={isAddBrackets} onChange={(e) => {
+                            setIsAddBrackets(e.target.checked);
+                            setIsAddParenthesis(false);
+                        }}/>
+                    </label>
+                </section>
+
                 <button id="transform-btn" onClick={transform} disabled={isProcessing}>
-                    { isProcessing ? "Working on it..." : "Transform" }
+                    {isProcessing ? "Working on it..." : "Transform"}
                 </button>
             </section>
         </>
